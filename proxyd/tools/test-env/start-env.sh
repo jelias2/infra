9971a692f8d6b36017a15cc56bd09ca6ce2cf84e
Jacob Elias <jacobelias.gv@gmail.com>
2024-07-17 10:26:34 -0500

    feat: adding remaining test files

---
+++ proxyd/tools/test-env/start-env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Source your environment variables
+source .env
+
+# Iterate through all arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        build)
+            echo "build argument detected"
+            echo "building proxyd..."
+            pushd ../../
+            make proxyd
+            cd ..
+            docker buildx build -f ./proxyd/Dockerfile -t ${IMAGE_NAME}:${IMAGE_TAG} . --platform linux/arm64
+            popd
+            echo "build complete"
+            ;;
+        *)
+            # Handle other arguments if needed
+            ;;
+    esac
+    shift # Move to the next argument
+done
+
+
+# Prepare the config file
+envsubst < ./proxyd/proxyd/proxyd.toml.template > ./proxyd/proxyd/proxyd.toml
+envsubst < ./proxyd/upstream-proxyd-1/template.proxyd.toml > ./proxyd/upstream-proxyd-1/proxyd.toml
+envsubst < ./proxyd/upstream-proxyd-2/template.proxyd.toml > ./proxyd/upstream-proxyd-2/proxyd.toml
+envsubst < ./proxyd/upstream-proxyd-3/template.proxyd.toml > ./proxyd/upstream-proxyd-3/proxyd.toml
+
+
+# Start Docker Compose
+echo "deploying..."
+docker-compose up -d --force-recreate
+echo "deployment complete"
